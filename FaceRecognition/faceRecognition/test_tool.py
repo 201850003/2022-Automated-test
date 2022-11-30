d146e263a922d409a54ceab0fb97f59b1956f01b
Faun <201850003@smail.nju.edu.cn>
2022-11-30 11:26:48 +0800

    test_tool可以输出识别错误的图片名称列表

--- FaceRecognition/faceRecognition/test_tool.py
+++ FaceRecognition/faceRecognition/test_tool.py
@@ -21,14 +21,13 @@ def print_line(delim_char, times, on_top):
 
 
 # 获取测试数据集中所有图片的名称
-def get_actual_res(name_list, path):
+def get_actual_res(name_list, actual_name_list):
     res = []
 
-    actual_name_list = read_name_list(path)
     for i in range(0, len(actual_name_list)):
-        actual_name = re.split(r'\d', actual_name_list[i])[0]
-        if actual_name in name_list:
-            res.append(actual_name)
+        one_actual_name = re.split(r'\d', actual_name_list[i])[0]
+        if one_actual_name in name_list:
+            res.append(one_actual_name)
         else:
             res.append('not find')
 
@@ -52,34 +51,45 @@ def test_model(name_list, img_path):
     return res_name, res_prob
 
 
-def eval_accuracy(test_list, actual_list, prob_list, predict_threshold):
+# 计算准确率
+def eval_accuracy(test_list, actual_list, actual_name_list, prob_list, predict_threshold):
     n_r = 0                     # 正确识别人脸的数量
     n_c = len(actual_list)      # 文件夹下图片总数
+    not_find_list = []
 
     for k in range(0, n_c):
         if test_list[k] == actual_list[k] and prob_list[k] >= predict_threshold:
             n_r += 1
+        else:
+            not_find_list.append(actual_name_list[k])
 
-    rate = '{:.2%}'.format(n_r / n_c)
+    rate = n_r/n_c
+    output(rate, predict_threshold, not_find_list)
+
+
+# 规范输出格式
+def output(accuracy, predict_threshold, not_find_list):
     if predict_threshold == 0:
-        print_line("=", 20, True)
+        print_line("=", 50, True)
     else:
-        print_line("*", 20, True)
-    print("The accuracy rate is: ")
-    print(rate)
-    print("The prediction threshold is: ")
-    print("{:.2f}".format(predict_threshold))
-    print()
+        print_line("*", 50, True)
+    print("The accuracy rate is: {:.2%}.".format(accuracy))
+    print("The prediction threshold is: {:.2f}".format(predict_threshold))
+    print("The following is a list of images that identify errors: ")
+    for not_find_name in not_find_list:
+        print(not_find_name)
 
 
 if __name__ == '__main__':
     origin_img_path = 'D:\\大三上学期课件\\FaceRecognition\\FaceRecognition\\input\\pins-face-recognition\\105_classes_pins_dataset'
     handle_img_path = '..\\pictures\\dataSet'
 
-    existing_name_list = handle_name_list(origin_img_path)
-    actual_res = get_actual_res(existing_name_list, handle_img_path)
-    test_res, test_res_prob = test_model(existing_name_list, handle_img_path)
+    existing_name = handle_name_list(origin_img_path)   # 获取人脸识别库中已录入的人脸对应人名
+    actual_name = read_name_list(handle_img_path)       # 获取测试集中所有图片名称
+    actual_res = get_actual_res(existing_name, actual_name)                 # 获取人脸识别的标准答案
+    test_res, test_res_prob = test_model(existing_name, handle_img_path)    # 获取模型测试结果
 
-    for j in np.arange(0, 0.7, 0.1):
-        eval_accuracy(test_res, actual_res, test_res_prob, j)
-    print_line("=", 20, False)
+    for j in np.arange(0.3, 0.7, 0.1):
+        eval_accuracy(test_res, actual_res, actual_name, test_res_prob, j)
+
+    print_line("=", 50, False)
